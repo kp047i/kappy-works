8c4a35374cdf2a4baf980fb9f5fa52eb13e0ed20
Takayuki Miura <takayuki.miura28@gmail.com>
2024-01-07 17:52:39 +0900

    feat: add projects page

---
+++ src/pages/projects/_consts/master.ts
@@ -0,0 +1,24 @@
+export const PROJECTS = [
+  {
+    title: "餅打",
+    description:
+      "Reactで構築したタイピングゲーム。卯年とお正月に社内で遊んでもらうように構築。",
+    emoji: "🐇",
+    repositoryLink: "https://github.com/kp047i/mochida",
+    appLink: "https://kp047i.github.io/mochida/",
+  },
+  {
+    title: "未来のなんじゃもんじゃ",
+    description:
+      "石巻ハッカソン2023で作成したボードゲームのなんじゃもんじゃをカスタマイズしたもの。",
+    emoji: "🃏",
+    repositoryLink: "https://github.com/kp047i/nanjamonja-future-front",
+  },
+  {
+    title: "kappy works",
+    description: "このサイト。",
+    emoji: "🧡",
+    repositoryLink: "https://github.com/kp047i/kappy-works",
+    appLink: "https://kappy.me/",
+  },
+] as const;
